7c8e1c1748a09cec88c694a0f5d5fb69cb7e1573
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-10-12 10:46:52 +0200

    Intermediate commit

--- pm4py/visualization/bpmn/factory.py
+++ pm4py/visualization/bpmn/factory.py
@@ -11,8 +11,10 @@ VERSIONS = {WO_DECORATION: wo_decoration.apply, FREQUENCY_DECORATION: frequency.
             PERFORMANCE_DECORATION: performance.apply, FREQUENCY_GREEDY: frequency.apply,
             PERFORMANCE_GREEDY: performance.apply}
 
+VERSIONS_PETRI = {WO_DECORATION: wo_decoration.apply_petri, FREQUENCY_DECORATION: wo_decoration.apply_petri, PERFORMANCE_DECORATION: wo_decoration.apply_petri,
+                  FREQUENCY_GREEDY: wo_decoration.apply_petri, PERFORMANCE_GREEDY: wo_decoration.apply_petri}
 
-def apply(bpmn_graph, parameters=None, variant="wo_decoration"):
+def apply(bpmn_graph, bpmn_aggreg_statistics=None, parameters=None, variant="wo_decoration"):
     """
     Factory method to visualize a BPMN graph using the given parameters
 
@@ -20,6 +22,8 @@ def apply(bpmn_graph, parameters=None, variant="wo_decoration"):
     -----------
     bpmn_graph
         BPMN graph object
+    bpmn_aggreg_statistics
+        Element-wise statistics that should be represented on the BPMN graph
     parameters
         Possible parameters, of the algorithm, including:
             format -> Format of the image to render (pdf, png, svg)
@@ -29,4 +33,31 @@ def apply(bpmn_graph, parameters=None, variant="wo_decoration"):
     file_name
         Path of the figure in which the rendered BPMN has been saved
     """
-    return VERSIONS[variant](bpmn_graph, parameters=parameters)
+    return VERSIONS[variant](bpmn_graph, bpmn_aggreg_statistics=bpmn_aggreg_statistics, parameters=parameters)
+
+def apply_petri(net, initial_marking, final_marking, log=None, bpmn_aggreg_statistics=None, parameters=None, variant="wo_decoration"):
+    """
+    Visualize a BPMN graph from a Petri net using the given parameters
+
+    Parameters
+    -----------
+    net
+        Petri net
+    initial_marking
+        Initial marking
+    final_marking
+        Final marking
+    log
+        (Optional) log where the replay technique should be applied
+    bpmn_aggreg_statistics
+        (Optional) element-wise statistics that should be represented on the BPMN graph
+    parameters
+        Possible parameters of the algorithm, including:
+            format -> Format of the image to render (pdf, png, svg)
+
+    Returns
+    -----------
+    file_name
+        Path of the figure in which the rendered BPMN has been saved
+    """
+    return VERSIONS_PETRI[variant](net, initial_marking, final_marking, log=log, bpmn_aggreg_statistics=bpmn_aggreg_statistics, parameters=parameters)
--- pm4py/visualization/bpmn/versions/wo_decoration.py
+++ pm4py/visualization/bpmn/versions/wo_decoration.py
@@ -1,6 +1,7 @@
 from pm4py.visualization.bpmn.util.bpmn_to_figure import bpmn_diagram_to_figure
+from pm4py.objects.conversion.bpmn_to_petri import factory as bpmn_converter
 
-def apply(bpmn_graph, parameters=None):
+def apply(bpmn_graph, parameters=None, bpmn_aggreg_statistics=None):
     """
     Visualize a BPMN graph from a BPMN graph using the given parameters
 
@@ -8,6 +9,8 @@ def apply(bpmn_graph, parameters=None):
     -----------
     bpmn_graph
         BPMN graph object
+    bpmn_aggreg_statistics
+        Element-wise statistics that should be represented on the BPMN graph
     parameters
         Possible parameters, of the algorithm, including:
             format -> Format of the image to render (pdf, png, svg)
@@ -21,6 +24,45 @@ def apply(bpmn_graph, parameters=None):
         parameters = {}
 
     format = parameters["format"] if "format" in parameters else "png"
+
+    file_name = bpmn_diagram_to_figure(bpmn_graph, format, bpmn_aggreg_statistics=None)
+    return file_name
+
+
+def apply_petri(net, initial_marking, final_marking, log=None, bpmn_aggreg_statistics=None, parameters=None):
+    """
+    Visualize a BPMN graph from a Petri net using the given parameters
+
+    Parameters
+    -----------
+    net
+        Petri net
+    initial_marking
+        Initial marking
+    final_marking
+        Final marking
+    log
+        (Optional) log where the replay technique should be applied
+    bpmn_aggreg_statistics
+        (Optional) element-wise statistics that should be represented on the BPMN graph
+    parameters
+        Possible parameters of the algorithm, including:
+            format -> Format of the image to render (pdf, png, svg)
+
+    Returns
+    -----------
+    file_name
+        Path of the figure in which the rendered BPMN has been saved
+    """
+
+    if parameters is None:
+        parameters = {}
+
+    format = parameters["format"] if "format" in parameters else "png"
+
+    bpmn_graph, elements_correspondence, inv_elements_correspondence, el_corr_keys_map = bpmn_converter.apply(net,
+                                                                                                              initial_marking,
+                                                                                                              final_marking)
 
     file_name = bpmn_diagram_to_figure(bpmn_graph, format, bpmn_aggreg_statistics=None)
     return file_name
