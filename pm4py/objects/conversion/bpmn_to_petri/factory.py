e189a21e09469599630fd4dc1f0ca791ac57932c
Alessandro Berti <a.berti@pads.rwth-aachen.de>
2018-10-24 13:44:00 +0200

    Intermediate commit (BPMN to Petri conversion)

--- pm4py/objects/conversion/bpmn_to_petri/factory.py
+++ pm4py/objects/conversion/bpmn_to_petri/factory.py
@@ -6,4 +6,27 @@ VERSIONS = {CLASSIC: classic.apply}
 
 
 def apply(bpmn_graph, parameters=None, variant="classic"):
-    return VERSIONS[CLASSIC](bpmn_graph, parameters=parameters)
+    """
+    Apply conversion from a BPMN graph to a Petri net
+    along with an initial and final marking
+
+    Parameters
+    -----------
+    bpmn_graph
+        BPMN graph
+    parameters
+        Parameters of the algorithm
+    variant
+        Variant of the algorithm to use, possible values:
+            classic
+
+    Returns
+    -----------
+    net
+        Petri net
+    initial_marking
+        Initial marking of the Petri net
+    final_marking
+        Final marking of the Petri net
+    """
+    return VERSIONS[variant](bpmn_graph, parameters=parameters)
--- pm4py/objects/conversion/bpmn_to_petri/versions/classic.py
+++ pm4py/objects/conversion/bpmn_to_petri/versions/classic.py
@@ -2,26 +2,125 @@ from pm4py.objects.petri import utils
 from pm4py.objects.petri.petrinet import PetriNet, Marking
 
 
+def remove_unconnected_places(net):
+    """
+    Remove unconnected places from the Petri net
+
+    Parameters
+    -----------
+    net
+        Petri net
+
+    Returns
+    -----------
+    net
+        Petri net
+    """
+    places = set(net.places)
+    for place in places:
+        if (len(place.in_arcs) == 0) and (len(place.out_arcs) == 0):
+            print("unconnected place: " + place.name)
+            net.places.remove(place)
+    return net
+
+
+def get_initial_marking(net):
+    """
+    Get the initial marking from a Petri net
+    (observing which nodes are without input connection)
+
+    Parameters
+    -----------
+    net
+        Petri net
+
+    Returns
+    -----------
+    initial_marking
+        Initial marking
+    """
+    places = set(net.places)
+    initial_marking = Marking()
+
+    for place in places:
+        if len(place.in_arcs) == 0:
+            initial_marking[place] = 1
+
+    return initial_marking
+
+
+def get_final_marking(net):
+    """
+    Get the final marking from a Petri net
+    (observing which nodes are without output connection,
+    if several nodes exist, then a sink place is created artificially)
+
+    Parameters
+    -------------
+    net
+        Petri net
+
+    Returns
+    -------------
+    net
+        Petri net
+    final_marking
+        Final marking of the Petri net
+    """
+    places = set(net.places)
+    places_wo_output = []
+    for place in places:
+        if len(place.out_arcs) == 0:
+            places_wo_output.append(place)
+
+    ftranscount = 0
+    final_marking = Marking()
+    if len(places_wo_output) > 1:
+        sink = PetriNet.Place('sink')
+        net.places.add(sink)
+        for place in places_wo_output:
+            ftranscount = ftranscount + 1
+            htrans = PetriNet.Transition("ftrans_" + str(ftranscount), None)
+            net.transitions.add(htrans)
+            utils.add_arc_from_to(place, htrans, net)
+            utils.add_arc_from_to(htrans, sink, net)
+        final_marking[sink] = 1
+    elif len(places_wo_output) == 1:
+        final_marking[places_wo_output[0]] = 1
+
+    return net, final_marking
+
+
 def apply(bpmn_graph, parameters=None):
+    """
+    Apply conversion from a BPMN graph to a Petri net
+    along with an initial and final marking
+
+    Parameters
+    -----------
+    bpmn_graph
+        BPMN graph
+    parameters
+        Parameters of the algorithm
+
+    Returns
+    -----------
+    net
+        Petri net
+    initial_marking
+        Initial marking of the Petri net
+    final_marking
+        Final marking of the Petri net
+    """
     if parameters is None:
         parameters = {}
-
     net = PetriNet("converted_net")
-    initial_marking = Marking()
-    final_marking = Marking()
-
     nodes = bpmn_graph.get_nodes()
-
-    places_count = 0
-    trans_count = 0
-
     corresponding_in_nodes = {}
     corresponding_out_nodes = {}
     corresponding_arcs = {}
-
     start_event_subprocess = {}
     end_event_subprocess = {}
-
     # adds nodes
     for node in nodes:
         node_id = node[1]['id']
@@ -29,140 +128,81 @@ def apply(bpmn_graph, parameters=None):
             1] else None
         node_type = node[1]['type'].lower()
         node_process = node[1]['process'].lower()
-
         if "task" in node_type:
-            trans_count = trans_count + 1
-            trans = PetriNet.Transition('tasktrans_' + str(trans_count), node_name)
+            trans = PetriNet.Transition(node_id, node_name)
             net.transitions.add(trans)
         elif "gateway" in node_type:
-            if "exclusivegateway" in node_type:
-                places_count = places_count + 1
-                input_place = PetriNet.Place('exclgatinppl_' + str(places_count))
-                net.places.add(input_place)
-
-                places_count = places_count + 1
-                output_place = PetriNet.Place('exclgatoutpl_' + str(places_count))
-                net.places.add(output_place)
-
-                trans_count = trans_count + 1
-                trans = PetriNet.Transition(node_id, None)
-                net.transitions.add(trans)
-
-                utils.add_arc_from_to(input_place, trans, net)
-                utils.add_arc_from_to(trans, output_place, net)
-
-                corresponding_in_nodes[node_id] = [input_place] * len(node[1]['incoming'])
-                corresponding_out_nodes[node_id] = [output_place] * len(node[1]['outgoing'])
-            elif "parallelgateway" in node_type:
+            if "parallelgateway" in node_type:
                 if len(node[1]['incoming']) > len(node[1]['outgoing']):
                     print("PARALLEL SPLIT GATEWAY")
                 else:
                     print("PARALLEL JOIN GATEWAY")
-        elif node_type == "startevent":
-            #places_count = places_count + 1
-            if "subprocess" in node_name:
-                print("SUBPROCESSSSSSSSS")
-                source_place = PetriNet.Place('so_'+str(node_process))
-                net.places.add(source_place)
             else:
-                print("MAIN PROCESSSSSSS")
-                source_place = PetriNet.Place('so_'+str(node_id))
-                net.places.add(source_place)
-
+                input_place = PetriNet.Place('i_' + node_id)
+                net.places.add(input_place)
+                output_place = PetriNet.Place('o_' + node_id)
+                net.places.add(output_place)
+                trans = PetriNet.Transition(node_id, None)
+                net.transitions.add(trans)
+                utils.add_arc_from_to(input_place, trans, net)
+                utils.add_arc_from_to(trans, output_place, net)
+                corresponding_in_nodes[node_id] = [input_place] * len(node[1]['incoming'])
+                corresponding_out_nodes[node_id] = [output_place] * len(node[1]['outgoing'])
+        elif node_type == "startevent":
+            source_place = PetriNet.Place(node_id)
+            net.places.add(source_place)
             corresponding_in_nodes[node_id] = [source_place]
             corresponding_out_nodes[node_id] = [source_place]
-
             start_event_subprocess[node_process] = source_place
-
-            print("AA ",node_process, node[1])
         elif node_type == "endevent":
-            #places_count = places_count + 1
-            if "subprocess" in node_name:
-                sink_place = PetriNet.Place('si_'+str(node_process))
-                net.places.add(sink_place)
-            else:
-                sink_place = PetriNet.Place('si_'+str(node_id))
-                net.places.add(sink_place)
-
+            sink_place = PetriNet.Place(node_id)
+            net.places.add(sink_place)
             corresponding_in_nodes[node_id] = [sink_place]
             corresponding_out_nodes[node_id] = [sink_place]
-
             end_event_subprocess[node_process] = sink_place
-
-            print("BB ",node_process, node[1])
-
         elif "event" in node_type:
-            places_count = places_count + 1
-
-            input_place = PetriNet.Place('evp_' + str(places_count))
+            input_place = PetriNet.Place('i_' + node_id)
             net.places.add(input_place)
-
-            places_count = places_count + 1
-
-            output_place = PetriNet.Place('evp_' + str(places_count))
+            output_place = PetriNet.Place('o_' + node_id)
             net.places.add(output_place)
-
-            trans_count = trans_count + 1
             trans = PetriNet.Transition(node_id, None)
             net.transitions.add(trans)
-
             corresponding_in_nodes[node_id] = [input_place]
             corresponding_in_nodes[node_id] = [output_place]
-
             utils.add_arc_from_to(input_place, trans, net)
             utils.add_arc_from_to(trans, output_place, net)
-
-            print("CC ",node_process, node[1])
-        else:
-            print("node_id = ", node_id)
-            print("node_name = ", node_name)
-            print("node_type = ", node_type)
-            print("node_process = ", node_process)
-
         if "task" in node_type:
-            places_count = places_count + 1
-            input_place = PetriNet.Place('p_' + str(places_count))
+            input_place = PetriNet.Place('it_' + node_id)
             net.places.add(input_place)
-            places_count = places_count + 1
-            output_place = PetriNet.Place('p_' + str(places_count))
+            output_place = PetriNet.Place('ot_' + node_id)
             net.places.add(output_place)
-
             corresponding_in_nodes[node_id] = [input_place]
             corresponding_out_nodes[node_id] = [output_place]
-
             utils.add_arc_from_to(input_place, trans, net)
             utils.add_arc_from_to(trans, output_place, net)
-
     # iterate again for subprocesses
     for node in nodes:
         node_id = node[1]['id']
-        node_name = node[1]['node_name'].replace("\r", " ").replace("\n", " ").strip() if 'node_name' in node[
-            1] else None
         node_type = node[1]['type'].lower()
         node_process = node[1]['process'].lower()
-
         if node_type == "subprocess":
-            print(node_process)
             if node_process in start_event_subprocess and node_process in end_event_subprocess:
                 corresponding_in_nodes[node_id] = [start_event_subprocess[node_process]]
                 corresponding_out_nodes[node_id] = [end_event_subprocess[node_process]]
-
-                print("SUBPROCESS",node_process)
-
     flows = bpmn_graph.get_flows()
-
     for flow in flows:
+        flow_id = flow[2]['id']
         source_ref = flow[2]['sourceRef']
         target_ref = flow[2]['targetRef']
-
         if source_ref in corresponding_out_nodes and target_ref in corresponding_in_nodes:
-            trans_count = trans_count + 1
-            trans = PetriNet.Transition('flowtrans_' + str(trans_count), None)
+            trans = PetriNet.Transition(flow_id, None)
             net.transitions.add(trans)
-
             utils.add_arc_from_to(corresponding_out_nodes[source_ref].pop(0), trans, net)
             target_arc = utils.add_arc_from_to(trans, corresponding_in_nodes[target_ref].pop(0), net)
-
             corresponding_arcs[target_arc] = flow
+
+    net = remove_unconnected_places(net)
+    initial_marking = get_initial_marking(net)
+    net, final_marking = get_final_marking(net)
 
     return net, initial_marking, final_marking
